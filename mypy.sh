c663f0e658f0bc41f56c70a45b561bdad87dd871
hansung <hansung080@hanmail.net>
2026-06-20 16:34:46 +0900

    added SRC_DIR variable in mypy.sh

--- mypy.sh
+++ mypy.sh
@@ -4,6 +4,7 @@ S_RESET=$'\033[0m'
 S_BOLD=$'\033[1m'
 
 PROJECT_ROOT="$(dirname "$(readlink -f "$0")")"
+SRC_DIR="$PROJECT_ROOT"
 MYPY=("$PROJECT_ROOT/.venv/bin/mypy" --strict)
 
 if (($# > 0)); then
@@ -12,7 +13,7 @@ else
   shopt -u dotglob
 
   # A while loop is used instead of find -exec to print the success message only once at the end.
-  # find "$PROJECT_ROOT/"* -type f -name '*.py' -exec "${MYPY[@]}" {} \;
+  # find "$SRC_DIR/"* -type f -name '*.py' -exec "${MYPY[@]}" {} \;
 
   COUNT=0
   ERRORED=false
@@ -22,12 +23,12 @@ else
       "${MYPY[@]}" "$FILE" >&2
     fi
     ((++COUNT))
-  done < <(find "$PROJECT_ROOT/"* -type f -name '*.py' -print0)
+  done < <(find "$SRC_DIR/"* -type f -name '*.py' -print0)
 
   "$ERRORED" && exit 1
 
   if ((COUNT == 0)); then
-    echo "There are no .py[i] files in directory '$PROJECT_ROOT'"
+    echo "There are no .py[i] files in directory '$SRC_DIR'"
   elif ((COUNT == 1)); then
     echo "${S_BOLD}Success: no issues found in $COUNT source file$S_RESET"
   else
